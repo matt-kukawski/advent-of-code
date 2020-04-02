244c0946f701b2f8f94388314f73d9d1fc244b8e
matt kukawski <matt@sonoflilith.com>
2020-04-02 17:32:00 +0100

    codewars parens parser

--- code-wars/strAndArrManipulation.js
+++ code-wars/strAndArrManipulation.js
@@ -54,9 +54,87 @@ function openOrSenior(data){
     return data.map((arr) => (arr[0] > 54 && arr[1] > 7) ? 'Senior' : 'Open');
     // return output;
 }
-console.log(openOrSenior([[18, 20],[55, 8],[61, 12]]));
+// console.log(openOrSenior([[18, 20],[55, 8],[61, 12]]));
 
 // best pratice
 // function openOrSenior(data){
 //     return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
 // }
+
+// Valid Parentheses
+
+// function validParentheses(parens){
+//     const splitParens = parens.split('');
+//     // console.log(splitParens);
+//     const valid = true;
+
+//     const parenLoop = (arr) => {
+//         if (arr.length > 1) {
+//             for (i = 0; i < arr.length-1; i++) {
+//                 if (arr[i] === '(' && arr[i+1] === ')') {
+//                     splitParens.splice(i, 2);
+//                     // console.log(splitParens);
+//                 } else {
+//                     valid = false;
+//                 }
+//             }         
+//         } else if (arr.length === 1) {
+//             valid = false;
+//         }
+//     }
+//     while (valid === true && !splitParens.length === 0) {
+//         parenLoop(splitParens);
+//     }
+//     console.log(valid);
+//     return valid;
+// }
+// validParentheses("(())((()())()))((");
+
+function validParentheses(parens){
+    let valid = true;
+    const splitParens = parens.split('');
+    const splitParensMod = splitParens.length % 2;
+    if (splitParensMod !== 0) {
+        valid = false;
+    } 
+    else 
+    {
+        const parenLoop = (arr) => {
+            // console.log('parenLoop...');
+            if (arr.length > 1) {
+                for (i = 0; i < arr.length-1; i++) {
+                    if (arr[i] === '(' && arr[i+1] === ')') {
+                        splitParens.splice(i, 2);
+                        return;
+                    }
+                }
+                valid = false;
+                return;         
+            }
+        }
+
+        while (valid === true && splitParens.length !== 0) {
+            parenLoop(splitParens);
+        }
+    }
+    return valid;
+}
+// console.log('result: ',validParentheses("(())((()())())"));
+
+// best practice
+// function validParentheses(string){
+//     var tokenizer = /[()]/g, // ignores characters in between; parentheses are
+//         count = 0,           // pretty useless if they're not grouping *something*
+//         token;
+//     while(token = tokenizer.exec(string), token !== null){
+//        if(token == "(") {
+//           count++;
+//        } else if(token == ")") {
+//           count--;
+//           if(count < 0) {
+//              return false;
+//           }
+//        }
+//     }
+//     return count == 0;
+//  }
