110d8bf0e8ca1a17ea5783121eac4d567259b411
matt kukawski <matt@sonoflilith.com>
2020-04-28 19:05:34 +0100

    sudoku checker algorithm

--- code-wars/algorithmsArrays.js
+++ code-wars/algorithmsArrays.js
@@ -83,3 +83,67 @@ snail = function(array) {
         //     }
         //     return result;
         //   }
+
+// SUDOKU algorithmn
+// todo: create arrays of squares to check
+
+function doneOrNot(board){
+    const nums = [1,2,3,4,5,6,7,8,9];
+    const failureMsg = 'Try again';
+    const rowCheck = (collection) => {
+        console.log('collection:', collection);
+        for (let row of collection) {
+            for (let item of nums) {              
+                const result = row.includes(item);
+                console.log(`'item: ${item}, included: ${result}`);
+                // if (!result) return failureMsg;
+            }
+        }
+    }
+
+    const columnChecker = () => {
+        // const columnArr = new Array(9).fill([]);
+        
+        // console.log('columnArr[0]:', columnArr[0]);
+        const columnCreator = () => {
+            const columnArr = [[],[],[],[],[],[],[],[],[]];
+            for (let row of board) {
+                for (let i=0; i <= row.length-1; i++) {
+                    const arr = columnArr[i];
+                    // console.log('arr:', arr);
+                    const val = row[i];
+                    // console.log('val:', val);
+                    arr.push(val);
+                };
+            }
+            console.log('columnArr:', columnArr);
+            return columnArr;
+        }
+        const columns = columnCreator();
+        rowCheck(columns);
+        // console.log('columnArr:', columnArr);
+    }
+    // rowCheck(board);
+    // columnChecker();
+}
+
+/*
+doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]]);
+*/
+doneOrNot([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 0, 3, 4, 9],
+    [1, 0, 0, 3, 4, 2, 5, 6, 0],
+    [8, 5, 9, 7, 6, 1, 0, 2, 0],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 0, 1, 5, 3, 7, 2, 1, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 0, 0, 4, 8, 1, 1, 7, 9]]);
