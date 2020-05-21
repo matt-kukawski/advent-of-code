34f9c8e1da449e686a3f895a2d6fb8d9d4aca07b
matt kukawski <matt@sonoflilith.com>
2020-05-21 16:59:03 +0100

    compareArrays added

---
+++ code-wars/compareArrays.js
@@ -0,0 +1,25 @@
+const arr1 = ['a','b','c','d','e'];
+const arr2 = ['z','x','y','c'];
+
+function commonItem(arr1, arr2) {
+    const myObj = {};
+    for (let key of arr1) {
+        myObj[key] = true;
+    }
+    // console.log('myObj:', myObj);
+    for (let val of arr2) {
+        if (myObj[val] !== undefined) {
+            console.log(`${val} exists in both arrays`);
+            break;
+        }
+    }
+}
+
+// ALT solution
+function commonItem2(arr1, arr2) {
+    return arr1.some(item => arr2.includes(item))
+}
+
+commonItem(arr1, arr2);
+const commIt = commonItem2(arr1, arr2);
+console.log('commonItem2:', commIt);
