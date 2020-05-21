617a1f322dd82db3b3ae6f9130487ce43ffc7e93
matt kukawski <matt@sonoflilith.com>
2020-05-21 16:24:17 +0100

    sum8 added

---
+++ code-wars/sumEight.js
@@ -0,0 +1,34 @@
+const arr1 = [1,2,3,9];
+const arr2 = [1,4,2,4,5];
+
+const sum8 = (arr) => {
+    const sortedArr = arr.sort();
+    let start = 0;
+    let end = arr.length-1;
+
+    const sumMe = (a, z) => {
+        return parseInt(a) + parseInt(z);
+    }
+
+    let currSum;
+    
+    while (currSum !== 8) {
+        currSum = sumMe(sortedArr[start], sortedArr[end]);
+        console.log('currSum:', currSum);
+        if (sortedArr[start] > sortedArr[end]) {
+            console.log('no pair equals 8');
+            break;
+        } else if (currSum ===8) {
+            console.log('pair exists that equals 8');
+            break;
+        } else if (currSum > 8) {
+            end -= 1;
+            console.log('sortedArr[end]:', sortedArr[end]);
+        } else if (currSum < 8) {
+            start += 1;
+            console.log('sortedArr[start]:', sortedArr[start]);
+        }
+    }
+}
+// sum8(arr1);
+// sum8(arr2);
